4cd1cd0442c238b987cafcd16910c6aba030aef2
Brian Richter <brianorwhatever@gmail.com>
2020-04-11 17:29:32 -0700

    init

---
+++ interfaces/flutter/test/screens/main_nav_test.dart
@@ -0,0 +1,77 @@
+import 'package:aviary_one/constants.dart';
+import 'package:flutter/material.dart';
+import '../components/home.dart';
+
+class MainNav extends StatefulWidget {
+  @override
+  _MainNavState createState() => _MainNavState();
+}
+
+class _MainNavState extends State<MainNav> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>[
+    Home(),
+    Text(
+      'Credentials',
+      style: kOptionStyle,
+    ),
+    Text(
+      'Scan',
+      style: kOptionStyle,
+    ),
+    Text(
+      'Notifications',
+      style: kOptionStyle,
+    ),
+    Text(
+      'Settings',
+      style: kOptionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Aviary ID'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.code),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            title: Text('Credentials'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box),
+            title: Text('Scan'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            title: Text('Notifications'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            title: Text('Settings'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: kSelectedNavColor,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
